1fb9e7c92dc45023919b76706643e345804a2ae7
Gaston <Gaston@DESKTOP-Q182OMJ>
2024-05-05 17:17:49 -0300

    Inicio Ejercicio 6

---
+++ src/ar/edu/unju/fi/ejercicio06/model/FelinoDomestico.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.ejercicio06.model;
+
+public class FelinoDomestico {
+	private String nombre;
+	private byte edad;
+	private float peso;
+}
---
+++ src/ar/edu/unju/fi/ejercicio06/model/FelinoSalvaje.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.ejercicio06.model;
+
+public class FelinoSalvaje {
+	private String nombre;
+	private byte edad;
+	private float peso;
+}
